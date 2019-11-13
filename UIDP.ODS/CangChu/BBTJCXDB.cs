c5e2433c3f7eaed1c96e441aa04b50ac9a02cdd9
tengfei8771 <532050072@qq.com>
2019-11-13 17:11:41 +0800

    1.添加功能

---
+++ UIDP.ODS/CangChu/BBTJCXDB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using UIDP.UTILITY;
+
+namespace UIDP.ODS.CangChu
+{
+    public class BBTJCXDB
+    {
+        DBTool db = new DBTool("");
+        public DataTable GetFCLInfo(string Date)
+        {
+            string dateStr = string.Empty;
+            string dateStr1 = string.Empty;        
+            if (string.IsNullOrEmpty(Date))
+            {
+                dateStr = DateTime.Now.ToString("yyyyMM");
+                dateStr1 = dateStr.Substring(0, 4) + "01";
+            }
+            else
+            {
+                dateStr = Convert.ToDateTime(Date).ToString("yyyyMM");
+                dateStr1 = dateStr.Substring(0, 4) + "01";
+            }
+            string sql = "SELECT WERKS,WERKS_NAME," +
+                "NVL((SELECT SUM(JE) JE FROM CONVERT_CKJE " +
+                " WHERE substr( BUDAT_MKPF, 1, 6 ) ='" + dateStr + "' AND WERKS=a.WERKS GROUP BY WERKS,WERKS_NAME),0)AS DANGYUE,NVL(SUM(JE),0) AS LEIJI FROM CONVERT_CKJE a " +
+                " WHERE substr( BUDAT_MKPF, 1, 6 ) >='" + dateStr1 + "' AND substr( BUDAT_MKPF, 1, 6 ) <= '" + dateStr + "'" +
+                " GROUP BY WERKS,WERKS_NAME ORDER BY WERKS";
+            return db.GetDataTable(sql);
+        }
+    }
+}
---
+++ WZGX.WebAPI/Controllers/CangChu/BBTJCXController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UIDP.BIZModule.CangChu;
+
+namespace WZGX.WebAPI.Controllers.CangChu
+{
+    [Produces("application/json")]
+    [Route("BBTJCX")]
+    public class BBTJCXController : WebApiBaseController
+    {
+        BBTJCXModule BBTJ = new BBTJCXModule();
+        /// <summary>
+        /// 发出量统计查询
+        /// </summary>
+        /// <param name="Date">日期</param>
+        /// <param name="page">页数</param>
+        /// <param name="limit">每页条数</param>
+        /// <returns></returns>
+        [HttpGet("GetFCLInfo")]
+        public IActionResult GetFCLInfo(string Date, int page, int limit) => Ok(BBTJ.GetFCLInfo(Date, page, limit));
+    }
+}
